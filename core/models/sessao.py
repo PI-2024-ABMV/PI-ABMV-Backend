dfca1ec32d15ad448b37f1d5331100bd901940eb
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-10-09 14:42:28 -0300

    FEAT: Model tabela Sessao

---
+++ core/models/sessao.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from .filme import Filme
+from .sala import Sala
+
+class Sessao(models.Model):
+    horario = models.DateTimeField(null=False, blank=False)
+    filme = models.ForeignKey(Filme, on_delete=models.PROTECT)
+    sala = models.ForeignKey(Sala, on_delete=models.PROTECT)
