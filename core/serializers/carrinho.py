f0398e9a3fd064a3249fa05527f90d0ecb859377
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-11-22 22:35:06 -0300

    CHORE: Serialização do carrinho

---
+++ core/serializers/carrinho.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Carrinho
+
+class CarrinhoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Carrinho
+        fields = "__all__"
