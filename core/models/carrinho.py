a276c791393068821d0ecbc9509239187f36fc0b
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-12-03 09:14:29 -0300

    FIX: Remoção do post_migrate e alteração do host

--- core/models/carrinho.py
+++ core/models/carrinho.py
@@ -4,7 +4,5 @@ from .ingresso import Ingresso
 
 
 class Carrinho(models.Model):
-    ingresso = models.ForeignKey(Ingresso, on_delete=models.CASCADE)
     quantidade = models.IntegerField()
-
-
+    ingresso = models.ForeignKey(Ingresso, on_delete=models.CASCADE)
